1be312b7caf079ec18d759f1104f4a72043a4791
mohamedr69 <mohamedr69@outlook.com>
2024-05-13 05:52:06 +0300

    Singleton

---
+++ cpp/design_patterns/Singleton/01-Singleton.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+
+/*Singleton --> create only one instance from object*/
+
+/*Advantages --> save memory, compatibility be
+tween developers*/
+
+/*Creation --> we need to have static memver of class, private constructor and
+ * static factory methods */
+
+class Singleton {
+ protected:
+  Singleton() = default;
+
+ public:
+  int data;
+  static Singleton& get_instance() {
+    static Singleton instance;
+    return instance;
+  }
+};
+
+int main() {
+  // Singleton error; private constructor
+
+  Singleton& singleton = Singleton::get_instance();
+  singleton.data = 20;
+
+  std::cout << "Sigleton.data -->" << singleton.data << "\n";
+
+  Singleton& singleton2 = Singleton::get_instance();
+
+  std::cout << "Sigleton2.data -->" << singleton2.data << "\n";
+
+  Singleton::get_instance().data = 50;
+
+  std::cout << "Sigleton3.data -->" << Singleton::get_instance().data << "\n";
+
+  return 0;
+}
